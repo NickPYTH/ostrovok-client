c8a29b4aec76fb0472d4347a51fe3c9d61f8d828
Николай С <nicksrnk@gmail.com>
2025-09-30 17:44:06 +0500

    Страничка с отчетами подключена Жду доработку по заявкам гостя, тк там связь

--- src/service/InsperctionReportService.ts
+++ src/service/InsperctionReportService.ts
@@ -5,27 +5,33 @@ import {InspectionReportModel} from "entities/InspectionReportModel";
 export const inspectionReportAPI = createApi({
     reducerPath: 'inspectionReportAPI',
     baseQuery: fetchBaseQuery({
-        baseUrl: `${host}/api/inspectionReport`,
+        baseUrl: `${host}/api/inspection-reports`,
+        prepareHeaders: (headers, { getState }) => {
+            const token = localStorage.getItem('access');
+            if (token)
+                headers.set('authorization', `Bearer ${token}`);
+            return headers;
+        },
     }),
     tagTypes: ['inspectionReport'],
     endpoints: (build) => ({
         getAll: build.mutation<InspectionReportModel[], void>({
             query: () => ({
-                url: `/getAll`,
+                url: ``,
                 method: 'GET',
             }),
             invalidatesTags: ['inspectionReport']
         }),
         get: build.mutation<InspectionReportModel, number>({
             query: (id) => ({
-                url: `/get?id=${id}`,
+                url: `/${id}`,
                 method: 'GET',
             }),
             invalidatesTags: ['inspectionReport']
         }),
         update: build.mutation<InspectionReportModel, InspectionReportModel>({
             query: (body) => ({
-                url: `/update`,
+                url: ``,
                 method: 'POST',
                 body
             }),
@@ -33,7 +39,7 @@ export const inspectionReportAPI = createApi({
         }),
         create: build.mutation<InspectionReportModel, InspectionReportModel>({
             query: (body) => ({
-                url: `/create`,
+                url: ``,
                 method: 'POST',
                 body
             }),
